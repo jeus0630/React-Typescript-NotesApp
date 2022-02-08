a38c543b96a4025a03768475d90e03412a90c964
jewoo <jeus0630@designfever.com>
2022-02-08 16:52:38 +0900

    Docs : props

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,1 +1,36 @@
-export {}
+import React, {FunctionComponent} from 'react';
+import {Note} from "../models/note.model";
+import {Card, Button} from "react-bootstrap";
+
+interface OwnProps {
+    note: Note;
+    handleDelete: (id: string) => void;
+}
+
+type Props = OwnProps;
+
+const Notes: FunctionComponent<Props> = ({note, handleDelete}) => {
+
+    return (
+        <div className={"mb-3"}>
+            <Card style={{backgroundColor: note.color}}>
+                <Card.Body>
+                    <Card.Title>
+                        {note.title}
+                    </Card.Title>
+                    <Card.Text>
+                        {note.text}
+                    </Card.Text>
+                    <Card.Subtitle className={"text-muted"}>
+                        {note.date}
+                    </Card.Subtitle>
+                    <Button className={"mt-3"} variant={"danger"} onClick={()=>{handleDelete(note.id)}}>
+                        Delete
+                    </Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default Notes;
--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,18 +1,34 @@
-import React, { FunctionComponent } from 'react';
+import React, {FunctionComponent} from 'react';
+import {Note} from "../models/note.model";
+import Notes from "./Notes";
 
 interface OwnProps {
-
+    notes: Note[];
 }
 
 type Props = OwnProps;
 
-const NotesList: FunctionComponent<Props> = (props) => {
+const NotesList: FunctionComponent<Props> = ({notes}) => {
 
-  return (
+    const handleDelete = (id: string) => {
+        console.log(id);
+    }
+
+    const renderNotes = () => notes.map((note,idx) => {
+            return <Notes key={note.id} note={note} handleDelete={handleDelete}></Notes>
+        })
+
+
+    return (
         <>
             <h2 className={"mt-3"}>Notes</h2>
+            <div>
+                {
+                    renderNotes()
+                }
+            </div>
         </>
-  );
+    );
 };
 
 export default NotesList;
